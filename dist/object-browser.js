b025ee00cc885113c27beefc69b753b9adf6ba7d
Kevin Barabash <kevinb7@gmail.com>
2014-12-11 15:07:58 -0700

    updated object-browser so it's no longer a singleton, added test cases

--- dist/object-browser.js
+++ dist/object-browser.js
@@ -9,7 +9,10 @@ function createString(text) {
     return createSpan('"' + text + '"', "string");
 }
 function genPreview(value, stop) {
-    if (typeof (value) === "object") {
+    if (value === null) {
+        return createSpan(value, "null");
+    }
+    else if (typeof (value) === "object") {
         var text = "";
         if (value instanceof Array) {
             text += "Array[" + value.length + "]";
@@ -48,75 +51,83 @@ function genPreview(value, stop) {
         return createSpan(value, "undefined");
     }
 }
-var paths = {};
-function genPropsList(obj, parentName) {
-    var $root = $("<ul></ul>").addClass("props");
-    var keys = Object.keys(obj);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        if (typeof (obj[key]) === "object") {
-            var path = parentName ? parentName + ":" + key : key;
-            var $triangle;
-            if (paths[path]) {
-                $triangle = createSpan("\u25BC", "triangle");
-            }
-            else {
-                $triangle = createSpan("\u25B6", "triangle");
-            }
-            var $li = $("<li></li>").css({ position: "relative" }).text(": ").prepend($triangle, createLabel(key)).append(genPreview(obj[key], true));
-            if (paths[path]) {
-                $li.append(genPropsList(obj[key], path));
-            }
-            $root.append($li);
-            (function (key, val) {
-                $triangle.click(function () {
-                    var path = parentName ? parentName + ":" + key : key;
-                    var $ul;
-                    if ($(this).parent().find('> ul').length !== 0) {
-                        $ul = $(this).parent().find('> ul');
-                        $ul.toggle();
-                    }
-                    else {
-                        $ul = genPropsList(val, path);
-                        $(this).parent().append($ul);
-                    }
-                    if ($ul.is(':visible')) {
-                        paths[path] = true;
-                        $(this).text('\u25BC');
-                    }
-                    else {
-                        paths[path] = false;
-                        $(this).text('\u25B6');
-                    }
-                });
-            })(key, obj[key]);
-        }
-        else {
-            $root.append($("<li></li>").text(": ").prepend(createLabel(key)).append(genPreview(obj[key], true)));
-        }
-    }
-    if (obj instanceof Array) {
-        $root.append($("<li></li>").text(": ").prepend(createSpan("length", "read-only")).append(genPreview(obj.length)));
-    }
-    return $root;
-}
 var ObjectBrowser = (function () {
     function ObjectBrowser(container) {
         this.container = container;
         this.$container = $(container);
+        this.paths = {};
     }
     Object.defineProperty(ObjectBrowser.prototype, "object", {
         set: function (obj) {
             this.$container.empty();
-            this.$container.append(genPropsList(obj));
+            if (typeof (obj) === "object" && obj !== null) {
+                this.$container.append(this.genPropsList(obj));
+            }
         },
         enumerable: true,
         configurable: true
     });
     ObjectBrowser.prototype.getOpenPaths = function () {
-        return Object.keys(paths).filter(function (path) {
-            return paths[path];
+        return Object.keys(this.paths).filter(function (path) {
+            return this.paths[path];
         });
+    };
+    ObjectBrowser.prototype.genPropsList = function (obj, parentName) {
+        var $root = $("<ul></ul>").addClass("props");
+        var keys = Object.keys(obj);
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            if (typeof (obj[key]) === "object") {
+                if (obj[key] === null) {
+                    $root.append($("<li></li>").text(": ").prepend(createLabel(key)).append(genPreview(obj[key], true)));
+                }
+                else {
+                    var path = parentName ? parentName + ":" + key : key;
+                    var $triangle;
+                    if (this.paths[path]) {
+                        $triangle = createSpan("\u25BC", "triangle");
+                    }
+                    else {
+                        $triangle = createSpan("\u25B6", "triangle");
+                    }
+                    var $li = $("<li></li>").css({ position: "relative" }).text(": ").prepend($triangle, createLabel(key)).append(genPreview(obj[key], true));
+                    if (this.paths[path]) {
+                        $li.append(this.genPropsList(obj[key], path));
+                    }
+                    $root.append($li);
+                    var self = this;
+                    (function (key, val) {
+                        $triangle.click(function () {
+                            var path = parentName ? parentName + ":" + key : key;
+                            var $ul;
+                            if ($(this).parent().find('> ul').length !== 0) {
+                                $ul = $(this).parent().find('> ul');
+                                $ul.toggle();
+                            }
+                            else {
+                                $ul = self.genPropsList(val, path);
+                                $(this).parent().append($ul);
+                            }
+                            if ($ul.is(':visible')) {
+                                self.paths[path] = true;
+                                $(this).text('\u25BC');
+                            }
+                            else {
+                                self.paths[path] = false;
+                                $(this).text('\u25B6');
+                            }
+                        });
+                    })(key, obj[key]);
+                }
+            }
+            else {
+                $root.append($("<li></li>").text(": ").prepend(createLabel(key)).append(genPreview(obj[key], true)));
+            }
+        }
+        if (obj instanceof Array) {
+            $root.append($("<li></li>").text(": ").prepend(createSpan("length", "read-only")).append(genPreview(obj.length)));
+        }
+        return $root;
     };
     return ObjectBrowser;
 })();
--- src/object-browser.ts
+++ src/object-browser.ts
@@ -13,7 +13,9 @@ function createString(text) {
 }
 
 function genPreview(value: any, stop?: boolean): any {
-    if (typeof(value) === "object") {
+    if (value === null) {
+        return createSpan(value, "null");
+    } else if (typeof(value) === "object") {
         var text = "";
         if (value instanceof Array) {
             text += "Array[" + value.length + "]";
@@ -47,89 +49,102 @@ function genPreview(value: any, stop?: boolean): any {
     }
 }
 
-var paths = {};
-
-function genPropsList(obj: any, parentName?: string) {
-    var $root = $("<ul></ul>").addClass("props");
-
-    var keys = Object.keys(obj);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-
-        if (typeof(obj[key]) === "object") {
-            var path = parentName ? parentName + ":" + key : key;
-            var $triangle;
-            if (paths[path]) {
-                $triangle = createSpan("\u25BC", "triangle");
-            } else {
-                $triangle = createSpan("\u25B6", "triangle");
-            }
-            var $li = $("<li></li>")
-                .css({ position: "relative" })
-                .text(": ")
-                .prepend($triangle, createLabel(key))
-                .append(genPreview(obj[key], true));
-            if (paths[path]) {
-                $li.append(genPropsList(obj[key], path));
-            }
-            $root.append($li);
-            (function (key, val) {
-                $triangle.click(function () {
-                    var path = parentName ? parentName + ":" + key : key;
-                    var $ul;
-                    if ($(this).parent().find('> ul').length !== 0) {
-                        $ul = $(this).parent().find('> ul');
-                        $ul.toggle();
-                    } else {
-                        $ul = genPropsList(val, path);
-                        $(this).parent().append($ul);
-                    }
-                    if ($ul.is(':visible')) {
-                        paths[path] = true;
-                        $(this).text('\u25BC');
-                    } else {
-                        paths[path] = false;
-                        $(this).text('\u25B6');
-                    }
-                });
-            })(key, obj[key]);
-        } else {
-            $root.append(
-                $("<li></li>")
-                    .text(": ")
-                    .prepend(createLabel(key))
-                    .append(genPreview(obj[key], true))
-            );
-        }
-    }
-    if (obj instanceof Array) {
-        $root.append(
-            $("<li></li>")
-                .text(": ")
-                .prepend(createSpan("length", "read-only"))
-                .append(genPreview(obj.length))
-        );
-    }
-
-    return $root;
-}
 
 class ObjectBrowser {
     $container: JQuery;
+    paths: { [path:string]: boolean };
 
     constructor(public container: HTMLDivElement) {
         this.$container = $(container);
+        this.paths = {};
     }
 
     set object(obj) {
         this.$container.empty();
-        this.$container.append(genPropsList(obj));
+        if (typeof(obj) === "object" && obj !== null) {
+            this.$container.append(this.genPropsList(obj));
+        }
     }
 
     getOpenPaths() {
-        return Object.keys(paths).filter(function (path) {
-            return paths[path];
+        return Object.keys(this.paths).filter(function (path) {
+            return this.paths[path];
         });
+    }
+
+    private genPropsList(obj: any, parentName?: string) {
+        var $root = $("<ul></ul>").addClass("props");
+
+        var keys = Object.keys(obj);
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+
+            if (typeof(obj[key]) === "object") {
+                if (obj[key] === null) {
+                    $root.append(
+                        $("<li></li>")
+                            .text(": ")
+                            .prepend(createLabel(key))
+                            .append(genPreview(obj[key], true))
+                    );
+                } else {
+                    var path = parentName ? parentName + ":" + key : key;
+                    var $triangle;
+                    if (this.paths[path]) {
+                        $triangle = createSpan("\u25BC", "triangle");
+                    } else {
+                        $triangle = createSpan("\u25B6", "triangle");
+                    }
+                    var $li = $("<li></li>")
+                        .css({ position: "relative" })
+                        .text(": ")
+                        .prepend($triangle, createLabel(key))
+                        .append(genPreview(obj[key], true));
+                    if (this.paths[path]) {
+                        $li.append(this.genPropsList(obj[key], path));
+                    }
+                    $root.append($li);
+                    var self = this;
+                    (function (key, val) {
+                        $triangle.click(function () {
+                            var path = parentName ? parentName + ":" + key : key;
+                            var $ul;
+                            if ($(this).parent().find('> ul').length !== 0) {
+                                $ul = $(this).parent().find('> ul');
+                                $ul.toggle();
+                            } else {
+                                $ul = self.genPropsList(val, path);
+                                $(this).parent().append($ul);
+                            }
+                            if ($ul.is(':visible')) {
+                                self.paths[path] = true;
+                                $(this).text('\u25BC');
+                            } else {
+                                self.paths[path] = false;
+                                $(this).text('\u25B6');
+                            }
+                        });
+                    })(key, obj[key]);
+                }
+            } else {
+                $root.append(
+                    $("<li></li>")
+                        .text(": ")
+                        .prepend(createLabel(key))
+                        .append(genPreview(obj[key], true))
+                );
+            }
+        }
+        if (obj instanceof Array) {
+            $root.append(
+                $("<li></li>")
+                    .text(": ")
+                    .prepend(createSpan("length", "read-only"))
+                    .append(genPreview(obj.length))
+            );
+        }
+
+        return $root;
     }
 }
 
